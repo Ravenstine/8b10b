e33c06995366fa308c7a2fbc646a21a55f5d541c
Ben Titcomb <btitcomb@scpr.org>
2018-05-09 12:00:13 -0700

    Have decoder take running disparity into account.

--- test/decoder.test.js
+++ test/decoder.test.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const   Decoder        = require('../lib/decoder'),
-      { assert }       = require('chai'),
-        table          = require('../lib/full_8b10b'),
-        invalid        = require('../lib/known-invalid');
+const   Decoder  = require('../lib/decoder'),
+      { assert } = require('chai'),
+        table    = require('../lib/full_8b10b'),
+        invalid  = require('../lib/known-invalid');
 
-describe('encoder', function(){
+describe('decoder', function(){
   it('decodes 10b to 8b bytes', function(){
     const decoder = new Decoder();
     table.forEach(row => {
+      decoder.rd = -1;
       const negInput  = parseInt(row['RD_M(a_j)'], 2),
             negResult = decoder.decode(negInput);
       assert.isTrue(negResult === parseInt(row['8B_BYTE'], 16));
+      decoder.rd = 1;
       const posInput  = parseInt(row['RD_P(a_j)'], 2),
             posResult = decoder.decode(posInput);
       assert.isTrue(posResult === parseInt(row['8B_BYTE'], 16));
